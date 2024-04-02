e383e20330e74396ff5831b35c237e231004fc76
YisusJoe <yisusjoe.mx@gmail.com>
2024-04-01 22:54:18 -0700

    Se agrego logica 2 a 1, junto a archivo de monitoreo de entradas

--- Model/rangeBookModel.py
+++ Model/rangeBookModel.py
@@ -57,7 +57,7 @@ def getBloqueVenta(secciones,Libro_venta):
     cantidad = []
     retCantidad = []
     suma = 0
-    puntos =  []
+    longPrecio =  0
     listpuntos = []
     i = 0
     j = 0
@@ -72,6 +72,8 @@ def getBloqueVenta(secciones,Libro_venta):
         # print(v[1])     #DESCOMENTAR PARA REVISAR EL LIBRO DE ORDENES
     # print(Libro_venta)
 
+    longPrecio = len(precio[0])
+    # print(longPrecio)
     # toy haciendo la logica de la venta
 
     # print(len(secciones))
@@ -82,8 +84,8 @@ def getBloqueVenta(secciones,Libro_venta):
         if(i != len(secciones)-1):
             # print(f'Entre a poner la primera seccion {secciones[i]}')
             # print(f'seccion {secciones[i+1]}')
-            s = secciones[i].ljust(6,'0')
-            s2 = secciones[i+1].ljust(6,'0') 
+            s = secciones[i].ljust(longPrecio,'0')
+            s2 = secciones[i+1].ljust(longPrecio,'0') 
             # print(f'seccion s: {s}')
             # print(f'seccion s: {s2}')
             suma += float(cantidad[precio.index(s)])
@@ -140,34 +142,6 @@ def getBloqueVenta(secciones,Libro_venta):
     # print(bloqueMax)
     return bloqueMax
 
-
-    # for seccion in secciones:
-    #     # print(f'seccion = {seccion}')
-    #     precio.append(seccion)
-
-    #     for Lc in Libro_venta:
-
-    #         if(Lc[0].startswith(seccion)):
-    #             # print(f'precio = {Lc[0]}, cantidad = {Lc[1]}')
-    #             suma += float(Lc[1])
-
-    #     # print(round(suma,2))
-    #     cantidad.append(round(suma,2))
-    #     retCantidad.append(round(suma,2))
-    #     puntos = precio + cantidad
-    #     # print(f'puntos = {puntos}')
-    #     listpuntos.append(puntos)
-    #     puntos = []
-    #     precio = []
-    #     cantidad = []
-    #     suma = 0
-
-    # posicion = retCantidad.index(max(retCantidad))
-    # bloqueMax = listpuntos[posicion]
-
-    # # print(bloqueMax)
-    # return bloqueMax
-
 class MdlRangosEstrategia:
 
     def __init__(self,orderBook):
@@ -187,7 +161,7 @@ class MdlRangosEstrategia:
             bloque = getBloqueCompra(secciones,Libro)
         if(cv=='asks'):
             secciones = sorted(getSecciones_001(Libro))
-            # print(sorted(secciones, reverse=True))
+            # print(sorted(secciones, reverse=True))      #COMENTAR PARA EJECUTAR
             secciones = sorted(secciones, reverse=True)
             bloque = getBloqueVenta(secciones,Libro)
 
@@ -202,53 +176,8 @@ class MdlRangosEstrategia:
             bloque = getBloqueCompra(secciones,Libro)
         if(cv=='asks'):
             secciones = sorted(getSecciones_01(Libro))
-            # print(sorted(secciones, reverse=True))
+            # print(sorted(secciones, reverse=True))      #COMENTAR PARA EJECUTAR
             secciones = sorted(secciones, reverse=True)
             bloque = getBloqueVenta(secciones,Libro)
 
         return bloque
-
-    def getRangosEstrategia(self, orderBook):
-
-        self.rangoCompraA, self.cantidadCompraA = self.rangosAB(orderBook['bids'], self.rangeMinCompra, self.rangeMaxCompra, 0.001, 0)
-        self.rangoCompraB, self.cantidadCompraB = self.rangosAB(orderBook['bids'], self.rangeMinCompra, self.rangeMaxCompra, 0.01, 0)
-        self.rangoVentaA, self.cantidadVentaA = self.rangosAB(orderBook['asks'], self.rangeMinVenta, self.rangeMaxVenta, 0.001, 1)
-        self.rangoVentaB, self.cantidadVentaB = self.rangosAB(orderBook['asks'], self.rangeMinVenta, self.rangeMaxVenta, 0.01, 1)
-
-    def rangosAB(self, orderBook, range_min, range_max, gap, flag):
-
-        price = []
-        quantity = []
-        while range_min <= range_max:
-
-            allPrice = 0
-            for ubook in orderBook:
-
-                if(float(ubook[0]) > (range_min) and float(ubook[0]) <= (range_min+gap) and flag == 0):
-                    allPrice += float(ubook[1])
-
-                if(float(ubook[0]) > (range_min-gap) and float(ubook[0]) <= (range_min) and flag == 1):
-                    allPrice += float(ubook[1])
-
-            price.append(range_min)
-            quantity.append(allPrice)
-            range_min += gap
-
-        max_quantity = max(quantity)
-        index_price = quantity.index(max_quantity)
-
-        return round(price[index_price],3),  max_quantity
-
-    def rangeMaxMin(self,orderBook):
-
-        compras = sorted(orderBook['bids'])
-        ventas = orderBook['asks']
-
-        self.rangeMaxCompra = float(max(compras)[0])
-        self.rangeMinCompra = float(min(compras)[0])
-        self.rangeMaxVenta = float(max(ventas)[0])
-        self.rangeMinVenta = float(min(ventas)[0])
-        # print(f'El rango max de compra es: {self.rangeMaxCompra}')
-        # print(f'El rango min de compra es: {self.rangeMinCompra}')
-        # down_book_sell = min(self.ordenesVenta)[0]
-        # up_book_sell = max(self.ordenesVenta)[0]
