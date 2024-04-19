c041d8215b40ac648566263ca1a21dcc14a697a1
YisusJoe <yisusjoe.mx@gmail.com>
2024-04-19 00:18:52 -0600

    Se agrego segunda entrada

--- botOrdenesPrice.py
+++ botOrdenesPrice.py
@@ -69,26 +69,33 @@ def dosaUno(symbol, dolar_buy_a,dolar_buy_b,dolar_sell_a,dolar_sell_b):
 def getTickets():
 
     symbols = []
-    exchange_info = client.futures_exchange_info()
-    futures_symbols = [symbol['symbol'] for symbol in exchange_info['symbols']]
-    
-    # print(futures_symbols)
-    for symbol in futures_symbols:
-        ticker = client.futures_symbol_ticker(symbol=symbol)
-            
-        if(ticker):
-            precio = float(ticker['price'])
-            if(precio < 1):
-                # print(precio)
-                # print(ticker)
-                # print(f'El precio del activo {symbol} es: {precio}')
-                symbols.append(symbol)
+    try:
+        exchange_info = client.futures_exchange_info()
+        futures_symbols = [symbol['symbol'] for symbol in exchange_info['symbols']]
 
-    return symbols
+        # print(futures_symbols)
+        for symbol in futures_symbols:
+            ticker = client.futures_symbol_ticker(symbol=symbol)
+                
+            if(ticker):
+                precio = float(ticker['price'])
+                if(precio < 1):
+                    # print(precio)
+                    # print(ticker)
+                    # print(f'El precio del activo {symbol} es: {precio}')
+                    symbols.append(symbol)
+
+        return symbols
+    
+    except:
+        pass
 
 def filtroEntrada(E_dosUno, symbol):
     
-    ticker = client.futures_symbol_ticker(symbol=symbol)
+    try:
+        ticker = client.futures_symbol_ticker(symbol=symbol)
+    except:
+        pass
 
     if(ticker):
         precio = float(ticker['price'])
@@ -142,122 +149,165 @@ def getPrice(symbol):
   
 def main():
 
-    inicio = time.time()  
-    symbols = getTickets()
-    fin = time.time()
-    print(f'getTickets() duro: {fin - inicio}')
-    # print(symbols)
+      
+    if(getTickets()):
+        inicio = time.time()
+        symbols = getTickets()
+        fin = time.time()
+        print(f'getTickets() duro: {fin - inicio}')
+        # print(symbols)
     
-    for symbol in symbols:
-        # print(f'Activo {symbol}')
+        for symbol in symbols:
+            # print(f'Activo {symbol}')
+            
+            try:
+                inicio = time.time()
+                Obj_libro_A = Libro(symbol=symbol,limit=limit_A)
+                Obj_libro_B = Libro(symbol=symbol,limit=limit_B)
+                Libro_total_A = Obj_libro_A.getlibroTotal()
+                Libro_total_B = Obj_libro_B.getlibroTotal()
+
+                Obj_estrategia_A = RangosEstrategia(Libro_total_A)
+                Obj_estrategia_B = RangosEstrategia(Libro_total_B)
+                
+                CompraBloque_A = Obj_estrategia_A.getBloqueA('bids')
+                VentaBloque_A = Obj_estrategia_A.getBloqueA('asks')
+                VentaBloque_B = Obj_estrategia_B.getBloqueB('asks')
+                CompraBloque_B = Obj_estrategia_B.getBloqueB('bids')
+                fin = time.time()
+                print(f'El calculo de rangos duro: {fin - inicio}')
         
-        try:
-            inicio = time.time()
-            Obj_libro_A = Libro(symbol=symbol,limit=limit_A)
-            Obj_libro_B = Libro(symbol=symbol,limit=limit_B)
-            Libro_total_A = Obj_libro_A.getlibroTotal()
-            Libro_total_B = Obj_libro_B.getlibroTotal()
+                E_dosUno = dosaUno(symbol, float(CompraBloque_A[0]), float(CompraBloque_B[0]), float(VentaBloque_A[0]), float(VentaBloque_B[0]))
+                F_Entrada = filtroEntrada(E_dosUno, symbol = symbol)
+                # print(f'{F_Entrada}\n')
+                # print(f'{symbol} {E_dosUno[0]} E: {E_dosUno[1]} SL: {E_dosUno[2]} TP: {E_dosUno[3]}')
 
-            Obj_estrategia_A = RangosEstrategia(Libro_total_A)
-            Obj_estrategia_B = RangosEstrategia(Libro_total_B)
-            
-            CompraBloque_A = Obj_estrategia_A.getBloqueA('bids')
-            VentaBloque_A = Obj_estrategia_A.getBloqueA('asks')
-            VentaBloque_B = Obj_estrategia_B.getBloqueB('asks')
-            CompraBloque_B = Obj_estrategia_B.getBloqueB('bids')
-            fin = time.time()
-            print(f'El calculo de rangos duro: {fin - inicio}')
-    
-            E_dosUno = dosaUno(symbol, float(CompraBloque_A[0]), float(CompraBloque_B[0]), float(VentaBloque_A[0]), float(VentaBloque_B[0]))
-            F_Entrada = filtroEntrada(E_dosUno, symbol = symbol)
-            # print(f'{F_Entrada}\n')
-            # print(f'{symbol} {E_dosUno[0]} E: {E_dosUno[1]} SL: {E_dosUno[2]} TP: {E_dosUno[3]}')
-
-            # print(f'B_Venta {VentaBloque_B}')
-            # print(f'A_Venta {VentaBloque_A}')
-            # print(f'A_Compra {CompraBloque_A}')
-            # print(f'B_Compra {CompraBloque_B}\n')
-            
-            tipoEntrada = E_dosUno[0]
-            Entrada = E_dosUno[1]
-            StopLoss = E_dosUno[2]
-            TakeProfit = E_dosUno[3]
-
-            while(E_dosUno and F_Entrada):
+                print(f'B_Venta {VentaBloque_B}')
+                print(f'A_Venta {VentaBloque_A}')
+                print(f'A_Compra {CompraBloque_A}')
+                print(f'B_Compra {CompraBloque_B}\n')
                 
-                precio = getPrice(symbol)
-                
-                print(f'{symbol} | {tipoEntrada} | {precio} E: {Entrada} TP: {TakeProfit} SL: {StopLoss}')
-                # logWrite(f'Encontre {tipoEntrada} | {precio} E: {Entrada} TP: {TakeProfit} SL: {StopLoss}')
+                tipoEntrada = E_dosUno[0]
+                Entrada = E_dosUno[1]
+                StopLoss = E_dosUno[2]
+                TakeProfit = E_dosUno[3]
 
-                if(tipoEntrada == 'LONG'):
+                while(E_dosUno and F_Entrada):
                     
-                    if(precio <= Entrada):
-                        
-                        print('Entre a la operacion')
-                        logWrite(f'{symbol} | {tipoEntrada} | Entre a la operacion en {Entrada}, SL: {StopLoss} y TP: {TakeProfit}')
-                        
-                        while(True):
-                            
-                            precio = getPrice(symbol)
-                            
-                            print(f'{symbol} | {tipoEntrada} | {precio} E: {Entrada} TP: {TakeProfit} SL: {StopLoss}')
-                            
-                            if(precio >= TakeProfit):
-                                
-                                print("Gane la operacion")
-                                logWrite(f'Gane la operacion, llegue al TP: {TakeProfit}, entre en {Entrada}')
-                                break
-                            
-                            if(precio <= StopLoss):
-                                
-                                print("Perdi la operacion")
-                                logWrite(f'Perdi la operacion, llegue al SL: {StopLoss}, entre en {Entrada}\n')
-                                break
-                        
-                        break
-                                
+                    precio = getPrice(symbol)
+                    
+                    print(f'{symbol} | {tipoEntrada} | {precio} E: {Entrada} TP: {TakeProfit} SL: {StopLoss}')
+                    # logWrite(f'Encontre {tipoEntrada} | {precio} E: {Entrada} TP: {TakeProfit} SL: {StopLoss}')
 
-                    if(precio >= TakeProfit):
+                    if(tipoEntrada == 'LONG'):
                         
-                        print('No pude entrar a la operacion')
-                        logWrite(f'{symbol} | {tipoEntrada} | Llegue a {TakeProfit} sin entrar a la operacion en {Entrada}')
-                        break
-                    
-                if(tipoEntrada == 'SHORT'):
-                    if(precio >= Entrada):
-                        
-                        print('Entre a la operacion')
-                        logWrite(f'{symbol} | {tipoEntrada} | Entre a la operacion en {Entrada}, SL: {StopLoss} y TP: {TakeProfit}')
-                        
-                        while(True):
+                        if(precio <= Entrada):
                             
-                            precio = getPrice(symbol)
+                            print('Entre a la operacion')
+                            logWrite(f'{symbol} | {tipoEntrada} | Entre a la operacion en {Entrada}, SL: {StopLoss} y TP: {TakeProfit}')
                             
-                            print(f'{symbol} | {tipoEntrada} | {precio} E: {Entrada} TP: {TakeProfit} SL: {StopLoss}')
-                            
-                            if(precio <= TakeProfit):
+                            while(True):
                                 
-                                print("Gane la operacion")
-                                logWrite(f'Gane la operacion, llegue al TP: {TakeProfit}, entre en {Entrada}')
-                                break
-                            
-                            if(precio >= StopLoss):
+                                precio = getPrice(symbol)
                                 
-                                print("Perdi la operacion")
-                                logWrite(f'Perdi la operacion, llegue al SL: {StopLoss}, entre en {Entrada}')
-                                break
-                            
-                        break
+                                print(f'{symbol} | {tipoEntrada} | {precio} E: {Entrada} TP: {TakeProfit} SL: {StopLoss}')
+                                
+                                if(precio >= TakeProfit):
+                                    
+                                    print("Gane la operacion")
+                                    logWrite(f'Gane la operacion, llegue al TP: {TakeProfit}, entre en {Entrada}')
+                                    break
+                                
+                                if(precio <= StopLoss):
+                                    
+                                    # print("Perdi la operacion")
+                                    # logWrite(f'Perdi la operacion, llegue al SL: {StopLoss}, entre en {Entrada}\n')
+                                    Sl2 = StopLoss - (StopLoss * 0.01)
 
-                    if(precio <= TakeProfit):
+                                    while(True):
+                                        
+                                        precio = getPrice(symbol)
+
+                                        print("Segunda entrada")
+                                        logWrite(f'Segunda entrada {StopLoss}')
+                                        
+                                        if(precio >= TakeProfit):
+                                    
+                                            print("Gane la operacion")
+                                            logWrite(f'Gane la operacion, llegue al TP: {TakeProfit}, entre en {StopLoss} con SL2 en {Sl2}')
+                                            break
+
+                                        if(precio <= Sl2):
+                                    
+                                            print("Perdi la operacion")
+                                            logWrite(f'Perdi la operacion, llegue al SL2: {Sl2}, entre en {Entrada}')
+                                            break
+
+                                    break
+                            
+                            break
+                                    
+
+                        if(precio >= TakeProfit):
+                            
+                            print('No pude entrar a la operacion')
+                            logWrite(f'{symbol} | {tipoEntrada} | Llegue a {TakeProfit} sin entrar a la operacion en {Entrada}')
+                            break
                         
-                        print('No pude entrar a la operacion')
-                        logWrite(f'{symbol} | {tipoEntrada} | Llegue a {TakeProfit} sin entrar a la operacion en {Entrada}')
-                        break
-                    
-        except:
-            pass
+                    if(tipoEntrada == 'SHORT'):
+                        if(precio >= Entrada):
+                            
+                            print('Entre a la operacion')
+                            logWrite(f'{symbol} | {tipoEntrada} | Entre a la operacion en {Entrada}, SL: {StopLoss} y TP: {TakeProfit}')
+                            
+                            while(True):
+                                
+                                precio = getPrice(symbol)
+                                
+                                print(f'{symbol} | {tipoEntrada} | {precio} E: {Entrada} TP: {TakeProfit} SL: {StopLoss}')
+                                
+                                if(precio <= TakeProfit):
+                                    
+                                    print("Gane la operacion")
+                                    logWrite(f'Gane la operacion, llegue al TP: {TakeProfit}, entre en {Entrada}')
+                                    break
+                                
+                                if(precio >= StopLoss):
+                                    
+                                    # print("Perdi la operacion")
+                                    # logWrite(f'Perdi la operacion, llegue al SL: {StopLoss}, entre en {Entrada}')
+                                    Sl2 = StopLoss + (StopLoss * 0.01)
+
+                                    while(True):
+                                    
+                                        precio = getPrice(symbol)
+
+                                        print("Segunda entrada")
+                                        logWrite(f'Segunda entrada {StopLoss}')
+                                        
+                                        if(precio <= TakeProfit):
+                                    
+                                            print("Gane la operacion")
+                                            logWrite(f'Gane la operacion, llegue al TP: {TakeProfit}, entre en {StopLoss} con SL2 en {Sl2}')
+                                            break
+
+                                        if(precio >= Sl2):
+                                    
+                                            print("Perdi la operacion")
+                                            logWrite(f'Perdi la operacion, llegue al SL2: {Sl2}, entre en {Entrada}')
+                                            break
+                                    break
+                                
+                            break
+
+                        if(precio <= TakeProfit):
+                            
+                            print('No pude entrar a la operacion')
+                            logWrite(f'{symbol} | {tipoEntrada} | Llegue a {TakeProfit} sin entrar a la operacion en {Entrada}')
+                            break
+                        
+            except:
+                pass
         # except Exception as error:
         #     print(f'error: {error}')
 
